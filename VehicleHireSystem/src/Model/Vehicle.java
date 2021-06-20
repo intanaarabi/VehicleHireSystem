f6ce52d5da6be9678b01f2b5613e5909343713cd
intan <intan.abangarabi@gmail.com>
2021-06-21 00:04:30 +0800

    Vehicle abstract class.

--- VehicleHireSystem/src/Model/Vehicle.java
+++ VehicleHireSystem/src/Model/Vehicle.java
@@ -1,5 +1,39 @@
 package Model;
 
-public class Vehicle {
-
+public abstract class Vehicle {
+	
+	private String vehicleMake;
+	private String vehicleModel;
+	private int vehicleTopSpeed;
+	private String vehicleRegNo;
+	private int vehicleDailyHireRate;
+	
+	public Vehicle (String vehicleMake, String vehicleModel, int vehicleTopSpeed, String vehicleRegNo, int vehicleDailyHireRate) {
+		this.vehicleMake = vehicleMake;
+		this.vehicleModel = vehicleModel;
+		this.vehicleTopSpeed = vehicleTopSpeed;
+		this.vehicleRegNo = vehicleRegNo;
+		this.vehicleDailyHireRate = vehicleDailyHireRate;
+	}
+	
+	public String getVehicleMake() {
+		return this.vehicleMake;
+	}
+	
+	public String getVehicleModel() {
+		return this.vehicleModel;
+	}
+	
+	public String getVehicleRegNo() {
+		return this.vehicleRegNo;
+	}
+	
+	public int getVehicleTopSpeed() {
+		return this.vehicleTopSpeed;
+	}
+	
+	public int getVehicleDailyHireRate() {
+		return this.vehicleDailyHireRate;
+	}
+	
 }
