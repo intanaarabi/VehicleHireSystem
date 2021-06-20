99611dbde43bcbf9cd48fbb74d17ebb59de7f681
intan <intan.abangarabi@gmail.com>
2021-06-21 00:24:02 +0800

    Added car

---
+++ VehicleHireSystem/src/Model/Car.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Car extends Vehicle {
+
+	public Car(String vehicleMake, String vehicleModel, int vehicleTopSpeed, String vehicleRegNo,
+			int vehicleDailyHireRate) {
+		super(vehicleMake, vehicleModel, vehicleTopSpeed, vehicleRegNo, vehicleDailyHireRate);
+		
+	}
+
+}
